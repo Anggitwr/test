3ba8e7bcdfbfdce4503027451794461754a5e396
anggitwr <agtwahyu23@gmail.com>
2023-04-05 12:25:49 +0700

    second

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('paginate_db','root','root',{
+const db = new Sequelize('paginate_db','root','',{
     host: "localhost",
     dialect: "mysql",
     user: 'root',
---
+++ frontend/src/component/UserList.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+
+const UserList = () => {
+    const [users, setUsers] = useState([]);
+    const [page, setPage] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [pages, setPages] = useState(0);
+    const [rows, setRows] = useState(0);
+    const [keyword, setKeyWords] = useState("");
+
+    useEffect(() => {
+        getUsers();
+    }, [page, keyword]);
+    const getUsers = async () => {
+        const response = await axios.get(
+            `http://localhost:5000/users?search_query=${keyword}&page=${page}&limit=${limit}`
+        );
+        setUsers(response.data.result);
+        setPage(response.data.page);
+        setPages(response.data.totalPage);
+        setRows(response.data.totalRows);
+    };
+
+    const changePage = ({ selected }) => {
+        setPage(selected);
+    };
+
+    return (
+        <div className="container mt-5">
+            <div className="columns">
+                <div className="column is-centered">
+                    <form>
+                        <div className="field has-addons">
+                            <div className="control is-expanded">
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="Find something here..."
+                                />
+                            </div>
+                            <div className="control">
+                                <button
+                                    type="submit"
+                                    className="button is-info">
+                                    Search
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    <table className="table is-striped is-bordered is-fullwidth mt-2">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Gender</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((user) => (
+                                <tr key={user.id}>
+                                    <td>{user.id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.gender}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <p>
+                        Total Rows: {rows} Page: {rows ? page + 1 : 0} of{" "}
+                        {pages}
+                    </p>
+                    <nav
+                        className="pagination is-centered"
+                        role="navigation"
+                        aria-label="pagination">
+                        <ReactPaginate
+                            previousLabel={"< Prev"}
+                            nextLabel={"Next >"}
+                            pageCount={pages}
+                            onPageChange={changePage}
+                            containerClassName={"pagination-list"}
+                            pageLinkClassName={"pagination-link"}
+                        />
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserList;
